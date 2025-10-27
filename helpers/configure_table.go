f618df649a3c23fcb6483782ac7356ec012c0798
Hossam Mourad <ihos4m@gmail.com>
2025-10-27 14:37:36 -0400

    improve table styling

--- helpers/configure_table.go
+++ helpers/configure_table.go
@@ -4,10 +4,17 @@ import (
 	"os"
 
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 func ConfigureTable(t table.Writer) {
 	t.SetOutputMirror(os.Stdout)
 	t.SetStyle(table.StyleRounded)
 	t.Style().Options.SeparateRows = true
+	t.Style().Options.DoNotColorBordersAndSeparators = true
+	t.Style().Color.RowAlternate = text.Colors{text.FgYellow}
+	t.Style().Color.Header = text.Colors{text.Bold}
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Number: 1, Colors: text.Colors{text.Bold}},
+	})
 }
