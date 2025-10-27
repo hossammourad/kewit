ef9aeaf0060c27d6014777caf64107536a713279
Hossam Mourad <ihos4m@gmail.com>
2025-10-26 20:35:32 -0400

    missing error print

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -14,6 +15,7 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
